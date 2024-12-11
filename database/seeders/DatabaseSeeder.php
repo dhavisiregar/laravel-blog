5ee68a6b593a69954476f9a4d70dd42ca3c61e77
Sultan Muhammad Dhavi <sultan.muhammaddhavi@cbncloud.co.id>
2024-12-11 14:23:07 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-design'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'artikel-1',
+        //     'body' => 'lorem ipsum dolor sit amet, consectetur adipis'
+        // ]);
+
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">About Us</h3>
+    <h3>Nama: {{ $nama }}</h3>
+  </x-layout>
