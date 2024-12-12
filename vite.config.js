5cdf0fc18c7cc666a6503805688bdfc210023f98
Sultan Muhammad Dhavi <sultan.muhammaddhavi@cbncloud.co.id>
2024-12-12 09:47:43 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,4 +5,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "dist", // Set the output directory to 'dist'
+        emptyOutDir: true, // Clean the directory before building
+    },
 });
